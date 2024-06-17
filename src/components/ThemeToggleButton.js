b83b8a16b19d5d3034d8cbe25478cb3f3a64d4b1
Hemant2122 <141365423+Hemant2122@users.noreply.github.com>
2024-06-17 16:40:41 +0530

    ThemeToggleButton

--- src/components/ThemeToggleButton.js
+++ src/components/ThemeToggleButton.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 
 const ThemeToggleButton = () =>{
-    
+    const { theme, toggleTheme } = useTheme();
     return (
-       <></>
+       <>
+            <button
+              id="global-theme-toggler"
+              className={`btn btn-${theme} txt-${theme}`}
+              onClick={toggleTheme}
+            >
+              Switch to {theme === 'light' ? 'dark' : 'light'} theme
+            </button>
+        </>
     )
 
 }
