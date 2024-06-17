050fa28aace63d4593ea6440ffe7cdadf2933df8
Hemant2122 <141365423+Hemant2122@users.noreply.github.com>
2024-06-17 16:46:05 +0530

    LocalThemedBox

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -3,9 +3,31 @@ import React from 'react';
 
 const LocalThemedBox = () => {
 
+  const { theme: globalTheme } = useTheme();
+  const [localTheme, setLocalTheme] = useState(globalTheme);
+
+  useEffect(() => {
+    setLocalTheme(globalTheme);
+  }, [globalTheme]);
+
+  const toggleLocalTheme = () => {
+    setLocalTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
 return(
-    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
+    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={`bg-${localTheme}`}>
         {/* Write code below this line */}
+
+        <p id="local-themed-text-container" className={`txt-${localTheme}`}>
+            This is a local themed text container.
+          </p>
+          <button
+            id="local-theme-toggler"
+            className={`btn btn-${localTheme}`}
+            onClick={toggleLocalTheme}
+          >
+            Toggle local theme to {localTheme === 'light' ? 'dark' : 'light'}
+          </button>
         
     </div>
 )
